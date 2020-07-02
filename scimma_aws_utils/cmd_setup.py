1851d66a253d2df6c3d23bb4cde50e74f0f5f3e4
Spencer Nelson <swnelson@uw.edu>
2020-07-02 12:02:23 -0700

    Refactor cmd_setup.py

--- scimma_aws_utils/cmd_setup.py
+++ scimma_aws_utils/cmd_setup.py
@@ -4,6 +4,8 @@ import requests
 import logging
 import sys
 
+from typing import Dict
+
 from .auth import login_aws_via_idp
 from .awscreds import parse_roles, write_aws_config
 from .configs import Config, default_config_file
@@ -33,6 +35,61 @@ def setup(config):
     ),
     )
     click.echo()
+    print_well_known_idps()
+    entity_id = click.prompt("Enter your IdP Entity ID", type=str).strip()
+
+    # Prompt for username and password
+    username = click.prompt(
+        "Username you use to login to your IdP", type=str).strip()
+    password = click.prompt("Password", hide_input=True, type=str)
+
+    region = click.prompt("AWS Region to connect to",
+                          type=str, default="us-west-2").strip()
+    profile_name = click.prompt(
+        "AWS credential profile name to use", type=str, default="default")
+    profile_name = profile_name.strip()
+
+    conf = Config(
+        entity_id=entity_id,
+        username=username,
+        password=password,
+        region=region,
+        profile_name=profile_name,
+    )
+
+    click.secho(
+        "Verifying your credentials by attempting to log in.", color="yellow")
+    assertion, roles = login_aws_via_idp(
+        requests.Session(), conf.username, conf.password, conf.entity_id,
+    )
+    click.secho("Log-in succesful!", color="green")
+
+    roles = parse_roles(roles)
+    if len(roles) == 1:
+        conf.role_arn = list(roles.keys())[0]
+    else:
+        conf.role_arn = prompt_for_role(roles)
+    click.echo(f"You will log in as {conf.role_arn}.")
+
+    conf.to_file(config)
+    click.echo(f"Your configuration has been saved to {config}.")
+    write_aws_config(conf.profile_name, conf.region, sys.argv[0])
+    click.echo("AWS configuration has been saved.")
+
+
+def prompt_for_role(roles: Dict[str, str]):
+    """ Prompt the user to pick one of the provided roles. """
+    click.echo("Choose role to assume")
+    enumerated = dict(enumerate(roles.keys(), start=1))
+    for i, role_arn in sorted(enumerated.items()):
+        click.echo(f"  [{i}]: {role_arn}")
+        chosen_idx = click.prompt(
+            "Choose role to assume", type=int,
+        )
+    return enumerated[chosen_idx]
+
+
+def print_well_known_idps():
     click.echo("Some well-known IdP entity IDs:")
     well_known_idps = {
         "Cornell University": "https://shibidp.cit.cornell.edu/idp/shibboleth",
@@ -46,47 +103,3 @@ def setup(config):
     for institution, entity in well_known_idps.items():
         click.secho(f"  {institution}", fg="yellow", nl=False)
         click.secho(f": {entity}")
-    entity_id = click.prompt("Enter your IdP Entity ID", type=str).strip()
-
-    # Prompt for username and password
-    username = click.prompt(
-        "Username you use to login to your IdP", type=str).strip()
-    password = click.prompt("Password", hide_input=True, type=str)
-
-    region = click.prompt("AWS Region to connect to",
-                          type=str, default="us-west-2").strip()
-    profile_name = click.prompt(
-        "AWS credential profile name to use", type=str, default="default")
-    profile_name = profile_name.strip()
-    # Write config file
-    conf = Config(
-        entity_id=entity_id,
-        username=username,
-        password=password,
-        region=region,
-        profile_name=profile_name,
-    )
-    click.secho(
-        "Verifying your credentials by attempting to log in.", color="yellow")
-    assertion, roles = login_aws_via_idp(
-        requests.Session(), conf.username, conf.password, conf.entity_id,
-    )
-    click.secho("Log-in succesful!", color="green")
-    roles = parse_roles(roles)
-    if len(roles) == 1:
-        conf.role_arn = list(roles.keys())[0]
-        click.echo(f"You will log in as {conf.role_arn}.")
-    else:
-        click.echo("Choose role to assume")
-        enumerated = dict(enumerate(roles.keys(), start=1))
-        for i, role_arn in sorted(enumerated.items()):
-            click.echo(f"  [{i}]: {role_arn}")
-        chosen_idx = click.prompt(
-            "Choose role to assume", type=int,
-        )
-        conf.role_arn = enumerated[chosen_idx]
-
-    conf.to_file(config)
-    click.echo(f"Your configuration has been saved to {config}.")
-    write_aws_config(conf.profile_name, conf.region, sys.argv[0])
-    click.echo("AWS configuration has been saved.")
